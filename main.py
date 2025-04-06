47bf4bd9c3c5b18a9682750f5c42c30a1ddf7c3d
Victor Jalba <jalbav.xc@gmail.com>
2025-04-05 22:34:16 -0700

    New changes in main and stats

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def get_book_text(pathfile):
-    with open(pathfile) as f:
-        file_content=f.read()
-    return file_content.split()
+from stats import  get_all_characters_count
 
-def main():
-    filepath = "bookbot/books/frankenstein.txt"
-    num_words=len(get_book_text(filepath))
-    print(f"{num_words} words found in the document")
 
-main()
+get_all_characters_count()
--- stats.py
+++ stats.py
@@ -1,4 +1,19 @@
+def get_book_text(pathfile):
+    with open(pathfile) as f:
+        file_content=f.read()
+    return file_content
+
 def get_num_words():
-    filepath = "bookbot/books/frankenstein.txt"
-    num_words=len(get_book_text(filepath.split()))
+    file = "books/frankenstein.txt"
+    num_words=len(get_book_text(filepath).split())
     print(f"{num_words} words found in the document")
+
+def get_all_characters_count():
+    filepath = "books/frankenstein.txt"
+    char_count={}
+    for char in get_book_text(filepath).lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    print(char_count)
