2835e40c4dd7ae0eb263d2b09040f5a9f8dd4bbf
Victor Jalba <jalbav.xc@gmail.com>
2025-04-06 19:31:46 -0700

    Finished product on bookbot

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from stats import  get_all_characters_count
+import sys
+from stats import sort_char_count, get_num_words
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+filepath = sys.argv[1]
 
-get_all_characters_count()
+print("============ BOOKBOT ============")
+print(sys.argv[1])
+print("----------- Word Count ----------")
+get_num_words(filepath)
+print("--------- Character Count -------")
+sort_char_count(filepath)
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,19 +1,29 @@
-def get_book_text(pathfile):
-    with open(pathfile) as f:
+def get_book_text(file):
+    with open(file) as f:
         file_content=f.read()
     return file_content
 
-def get_num_words():
-    file = "books/frankenstein.txt"
-    num_words=len(get_book_text(filepath).split())
-    print(f"{num_words} words found in the document")
+def get_num_words(file):
+    num_words=len(get_book_text(file).split())
+    print(f"Found {num_words} total words")
 
-def get_all_characters_count():
-    filepath = "books/frankenstein.txt"
+def get_all_characters_count(file):
     char_count={}
-    for char in get_book_text(filepath).lower():
+    for char in get_book_text(file).lower():
         if char in char_count:
             char_count[char] += 1
         else:
             char_count[char] = 1
-    print(char_count)
+    return char_count
+
+def sort_on(item):
+    return item[1]
+
+def sort_char_count(file):
+    char_count=get_all_characters_count(file)
+    filter_items=[item for item in char_count.items() if item[0].isalpha()]
+    sorted_chars=sorted(filter_items,reverse=True,key=sort_on)
+    for char,count in sorted_chars:
+        print(f"{char}: {count}")
+    return sorted_chars
+    
